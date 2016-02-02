6c3bfdfa06141271f332460f27587b5d641eda0b
ZJ <zj_jun@163.com>
2016-02-03 01:22:47 +0800

    Add rotate right in P55 向右旋转list(P55)

---
+++ 3_5_rotate_right.cc
@@ -0,0 +1,17 @@
+/*
+ * 3_5_rotate_right.cc
+ *
+ * Given a list, rotate the list to the right by k places, where k
+ * is non-negative. e.g: 1->2->3->4->5, k = 2; after rotation：
+ * 4->5->1->2->3
+ *
+ * 给定一个链表，向右旋转链表k个位置，其中k是一个非负数。例如， 1->2->3->4->5, k = 2;
+ * 旋转之后得到： 4->5->1->2->3。
+ *
+ *  Created on: Feb 3, 2016
+ *      Author: ZJ
+ */
+
+
+
+
